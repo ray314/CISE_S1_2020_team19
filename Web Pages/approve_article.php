76c3feff3405f5ef57d8c490f8cd8a0a99835865
ray314 <yeungj1602@gmail.com>
2020-05-11 20:29:51 +1200

    created home page and approve page

---
+++ Web Pages/approve_article.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <title>Approve article</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <h1>List of articles</h1>
+            <h2>Articles waiting to be approved</h2>
+        </div>
+        
+    </body>
+</html>
